eb0b1b61ae5f4f530a6cf3e0c1def74919a966ff
Teo Asinari <tasinari@gmail.com>
2019-10-04 15:34:33 -0700

    Adding chooseSwapSurfaceFormat

--- main.cpp
+++ main.cpp
@@ -328,6 +328,17 @@ private:
 		return details;
 	}
 
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+		for (const auto& availableFormat : availableFormats) {
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace ==
+				VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
+	}
+
 	void pickPhysicalDevice() {
 		uint32_t deviceCount = 0;
 		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
