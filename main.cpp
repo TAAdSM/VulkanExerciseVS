bbb91784a449923cb64ce4bb5c558b8b28824d80
Teo Asinari <tasinari@gmail.com>
2019-10-07 09:53:13 -0700

    Adding code read shader SPIR-V files and wrap them with VkShaderModules

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 #include <cstdint>
 #include <algorithm>
+#include <fstream>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -324,7 +325,28 @@ private:
 	}
 
 	void createGraphicsPipeline() {
+		std::vector<char> vertShaderCode = readFile("shaders/vert.spv");
+		std::vector<char> fragShaderCode = readFile("shaders/frag.spv");
 
+		std::cout << "The size of the vertShaderCode is: " << vertShaderCode.size() << std::endl;
+		std::cout << "The size of the fragShaderCode is: " << fragShaderCode.size() << std::endl;
+	}
+
+	static std::vector<char> readFile(const std::string& filename) {
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+
+		return buffer;
 	}
 
 	struct QueueFamilyIndices {
